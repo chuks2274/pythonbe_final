bc35aa0bcb1526805a620679da0d84a91d899714
Michael Chukwuka Meninwa <chukwuka4christ@yahoo.com>
2025-08-14 21:44:20 -0400

    Update config with DATABASE_URI and SECRET_KEY

--- application/config.py
+++ application/config.py
@@ -3,7 +3,8 @@ import os
 class Config:
     # Base configuration
     SECRET_KEY = os.getenv("SECRET_KEY", "default_secret")
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI", "sqlite:///db.sqlite3")
+    # Strip whitespace to avoid PostgreSQL errors
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI", "sqlite:///db.sqlite3").strip()
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class DevelopmentConfig(Config):
